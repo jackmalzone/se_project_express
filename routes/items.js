eb3542b5a99703c9f8b8487bea6db8cb2b175f38
JAK <56848280+jackmalzone@users.noreply.github.com>
2024-12-04 21:38:44 -0800

    refactor: standardize validation middleware in items routes

--- routes/items.js
+++ routes/items.js
@@ -7,12 +7,15 @@ const {
   likeItem,
   dislikeItem,
 } = require("../controllers/items");
-const validateObjectId = require("../middlewares/validateObjectId");
+const {
+  validateId,
+  validateClothingItem,
+} = require("../middlewares/validation");
 
 router.get("/", getItems);
-router.post("/", auth, createItem);
-router.delete("/:itemId", auth, validateObjectId, deleteItem);
-router.put("/:itemId/likes", auth, validateObjectId, likeItem);
-router.delete("/:itemId/likes", auth, validateObjectId, dislikeItem);
+router.post("/", auth, validateClothingItem, createItem);
+router.delete("/:itemId", auth, validateId, deleteItem);
+router.put("/:itemId/likes", auth, validateId, likeItem);
+router.delete("/:itemId/likes", auth, validateId, dislikeItem);
 
 module.exports = router;
